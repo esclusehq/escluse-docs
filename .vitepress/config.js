23171b107c0bbb600594fd29278162d06a01b991
Escluse Dev <dev@esluce.com>
2026-05-18 11:47:32 +0700

    feat: create dedicated About page, move content from index

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -8,35 +8,133 @@ export default defineConfig({
     logo: '/logo.svg',
     nav: [
       { text: 'Getting Started', link: '/getting-started/installation' },
+      { text: 'About', link: '/about/' },
       { text: 'API Reference', link: '/api/overview' }
     ],
-    sidebar: [
-      {
-        text: 'Getting Started',
-        items: [
-          { text: 'Introduction', link: '/' },
-          { text: 'Installation', link: '/getting-started/installation' },
-          { text: 'Quick Start', link: '/getting-started/quick-start' },
-          { text: 'Configuration', link: '/getting-started/configuration' }
-        ]
-      },
-      {
-        text: 'About Escluse',
-        items: [
-          { text: 'What is Escluse?', link: '/#what-is-escluse' },
-          { text: 'Feature Reference', link: '/#feature-reference' }
-        ]
-      },
-      {
-        text: 'API Reference',
-        items: [
-          { text: 'Overview', link: '/api/overview' },
-          { text: 'Servers', link: '/api/servers' },
-          { text: 'Nodes', link: '/api/nodes' },
-          { text: 'Billing', link: '/api/billing' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/': [
+        {
+          text: 'About Escluse',
+          collapsed: false,
+          items: [
+            { text: 'Overview', link: '/about/' },
+            { text: 'What is Escluse?', link: '/about/#what-is-escluse' },
+            { text: 'Feature Reference', link: '/about/#feature-reference' }
+          ]
+        },
+        {
+          text: 'Getting Started',
+          collapsed: false,
+          items: [
+            { text: 'Introduction', link: '/' },
+            { text: 'Installation', link: '/getting-started/installation' },
+            { text: 'Quick Start', link: '/getting-started/quick-start' },
+            { text: 'Configuration', link: '/getting-started/configuration' }
+          ]
+        },
+        {
+          text: 'API Reference',
+          items: [
+            { text: 'Overview', link: '/api/overview' },
+            { text: 'Servers', link: '/api/servers' },
+            { text: 'Nodes', link: '/api/nodes' },
+            { text: 'Billing', link: '/api/billing' }
+          ]
+        }
+      ],
+      '/getting-started/': [
+        {
+          text: 'About Escluse',
+          collapsed: false,
+          items: [
+            { text: 'Overview', link: '/about/' },
+            { text: 'What is Escluse?', link: '/about/#what-is-escluse' },
+            { text: 'Feature Reference', link: '/about/#feature-reference' }
+          ]
+        },
+        {
+          text: 'Getting Started',
+          collapsed: false,
+          items: [
+            { text: 'Introduction', link: '/' },
+            { text: 'Installation', link: '/getting-started/installation' },
+            { text: 'Quick Start', link: '/getting-started/quick-start' },
+            { text: 'Configuration', link: '/getting-started/configuration' }
+          ]
+        },
+        {
+          text: 'API Reference',
+          items: [
+            { text: 'Overview', link: '/api/overview' },
+            { text: 'Servers', link: '/api/servers' },
+            { text: 'Nodes', link: '/api/nodes' },
+            { text: 'Billing', link: '/api/billing' }
+          ]
+        }
+      ],
+      '/api/': [
+        {
+          text: 'About Escluse',
+          collapsed: false,
+          items: [
+            { text: 'Overview', link: '/about/' },
+            { text: 'What is Escluse?', link: '/about/#what-is-escluse' },
+            { text: 'Feature Reference', link: '/about/#feature-reference' }
+          ]
+        },
+        {
+          text: 'Getting Started',
+          collapsed: false,
+          items: [
+            { text: 'Introduction', link: '/' },
+            { text: 'Installation', link: '/getting-started/installation' },
+            { text: 'Quick Start', link: '/getting-started/quick-start' },
+            { text: 'Configuration', link: '/getting-started/configuration' }
+          ]
+        },
+        {
+          text: 'API Reference',
+          collapsed: false,
+          items: [
+            { text: 'Overview', link: '/api/overview' },
+            { text: 'Servers', link: '/api/servers' },
+            { text: 'Nodes', link: '/api/nodes' },
+            { text: 'Billing', link: '/api/billing' }
+          ]
+        }
+      ],
+      '/about/': [
+        {
+          text: 'About Escluse',
+          collapsed: false,
+          items: [
+            { text: 'Overview', link: '/about/' },
+            { text: 'What is Escluse?', link: '/about/#what-is-escluse' },
+            { text: 'Feature Reference', link: '/about/#feature-reference' }
+          ]
+        },
+        {
+          text: 'Getting Started',
+          collapsed: false,
+          items: [
+            { text: 'Introduction', link: '/' },
+            { text: 'Installation', link: '/getting-started/installation' },
+            { text: 'Quick Start', link: '/getting-started/quick-start' },
+            { text: 'Configuration', link: '/getting-started/configuration' }
+          ]
+        },
+        {
+          text: 'API Reference',
+          collapsed: false,
+          items: [
+            { text: 'Overview', link: '/api/overview' },
+            { text: 'Servers', link: '/api/servers' },
+            { text: 'Nodes', link: '/api/nodes' },
+            { text: 'Billing', link: '/api/billing' }
+          ]
+        }
+      ]
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/esclusehq' }
     ]
